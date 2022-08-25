07be53462f018dfb64e5d06fbc2476a80675bcdb
Yasir <yalmotaw2@huskers.unl.edu>
2022-08-25 00:36:22 +0000

    Thu, Aug 25, 2022, 12:36 AM +00:00

--- hello.c
+++ hello.c
@@ -1,3 +1,18 @@
   git add .gitignore
   git commit -m 'added gitignore file'
   git push -u origin main
+
+git commit -m 'initial commit'
+
+git branch -m main
+
+git remote add origin https://LOGIN:TOKEN@github.com/LOGIN/hello.git
+
+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
+
+YasirAlmotawa
+
+git push -u origin main
+
+git remote set-url origin https://LOGIN:TOKEN@github.com/LOGIN/hello.git
+
